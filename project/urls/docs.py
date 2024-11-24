7e85b11be3cfc392314a759c672e475e8021414b
anandrajB <anand98.ar@gmail.com>
2024-11-24 23:54:49 +0530

    updated statics

--- project/urls/docs.py
+++ project/urls/docs.py
@@ -2,6 +2,8 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,4 +21,4 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
